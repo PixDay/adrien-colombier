dbadf0eb8534c388239e75c2c0c54b9d35eb5dd9
PixDay <adrien.colombier@epitech.eu>
2020-10-26 10:21:58 +0100

    [MODIFIED] - Set HomeComponent as default root component

--- adrien-colombier/src/app/app-routing.module.ts
+++ adrien-colombier/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  //{ path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
